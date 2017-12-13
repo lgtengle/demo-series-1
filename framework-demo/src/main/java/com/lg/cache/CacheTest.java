6d431d147aa6582329677f54e5f6c148d6fffc34
leiguang <leiguang@richstonedt.com>
2017-12-13 20:49:12 +0800

    add:test cache expiry

--- framework-demo/src/main/java/com/lg/cache/CacheTest.java
+++ framework-demo/src/main/java/com/lg/cache/CacheTest.java
@@ -15,10 +15,20 @@ public class CacheTest {
 
     public static void main(String[] args) throws InterruptedException {
         CacheService cacheService = SpringContextInstance.getBean("cacheService", CacheService.class);
+        System.out.println(cacheService.getData("xiuxiuxiu"));
+        System.out.println("---------");
+
         System.out.println(cacheService.getData("hello world"));
+        System.out.println("-----------------");
+
         System.out.println(cacheService.getData("hello world"));
+        System.out.println("-------------------------------");
         Thread.sleep(7000);
+
+        //缓存过期
         System.out.println(cacheService.getData("hello world"));
+        System.out.println("----------------------------------------------");
+
 
     }
 
--- framework-demo/src/main/java/com/lg/cache/MyCache.java
+++ framework-demo/src/main/java/com/lg/cache/MyCache.java
@@ -18,6 +18,13 @@ public class MyCache implements Cache{
 
     Map<Object, Object> caches = new HashMap<>();
 
+    private long validTime;
+
+
+    public void setValidTime(long validTime) {
+        this.validTime = validTime  * 1000;
+    }
+
     @Override
     public String getName() {
         return "myCache";
@@ -31,7 +38,16 @@ public class MyCache implements Cache{
     @Override
     public ValueWrapper get(final  Object o) {
         System.out.println("读取缓存1");
-        return caches.get(o) == null ? null:new SimpleValueWrapper(caches.get(o));
+        Object result = caches.get(o);
+        if (result == null)
+            return null;
+        else{
+            MyValueWrapper myValueWrapper = (MyValueWrapper) caches.get(o);
+            if (System.currentTimeMillis() - myValueWrapper.getStartTime() > validTime)
+                return null;
+            else
+                return myValueWrapper;
+        }
     }
 
     @Override
@@ -43,7 +59,7 @@ public class MyCache implements Cache{
     @Override
     public void put(Object o, Object o1) {
         System.out.println("设置缓存");
-        caches.put(o, o1);
+        caches.put(o, new MyValueWrapper(o1));
     }
 
     @Override
@@ -61,5 +77,27 @@ public class MyCache implements Cache{
     @Override
     public void clear() {
         caches.clear();
+    }
+
+    static class MyValueWrapper extends SimpleValueWrapper{
+
+        private long startTime;
+
+        public MyValueWrapper(Object value) {
+            this(value, System.currentTimeMillis());
+        }
+
+        public MyValueWrapper(Object value, long startTime) {
+            super(value);
+            this.startTime = startTime;
+        }
+
+        public long getStartTime() {
+            return startTime;
+        }
+
+        public void setStartTime(long startTime) {
+            this.startTime = startTime;
+        }
     }
 }
